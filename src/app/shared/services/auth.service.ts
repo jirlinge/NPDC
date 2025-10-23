c3c760ab57de9b5785366b829c2d59e2082ece0a
jirlinge <julienirlinger@gmail.com>
2025-10-23 23:54:16 +0200

    Updated auth.service.ts

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -24,37 +24,48 @@ export class AuthService {
 
   // Profils en dur
   private readonly users: User[] = [
-    {
-      id: 1,
-      username: 'admin',
-      email: 'admin@npdc.com',
-      role: 'admin',
-      firstName: 'Admin',
-      lastName: 'User'
-    },
-    {
-      id: 2,
-      username: 'user',
-      email: 'user@npdc.com',
-      role: 'user',
-      firstName: 'Regular',
-      lastName: 'User'
-    },
-    {
-      id: 3,
-      username: 'demo',
-      email: 'demo@npdc.com',
-      role: 'user',
-      firstName: 'Demo',
-      lastName: 'User'
-    }
+    { id: 1, username: 'admin', email: 'admin@npdc.com', role: 'admin', firstName: 'Admin', lastName: 'User' },
+    { id: 2, username: 'alexandra', email: 'alexandra@npdc.com', role: 'user', firstName: 'Alexandra', lastName: '' },
+    { id: 3, username: 'felix', email: 'felix@npdc.com', role: 'user', firstName: 'Félix', lastName: '' },
+    { id: 4, username: 'guillaume', email: 'guillaume@npdc.com', role: 'user', firstName: 'Guillaume', lastName: '' },
+    { id: 5, username: 'sydney', email: 'sydney@npdc.com', role: 'user', firstName: 'Sydney', lastName: '' },
+    { id: 6, username: 'paul', email: 'paul@npdc.com', role: 'user', firstName: 'Paul', lastName: '' },
+    { id: 7, username: 'kevin', email: 'kevin@npdc.com', role: 'user', firstName: 'Kevin', lastName: '' },
+    { id: 8, username: 'marc', email: 'marc@npdc.com', role: 'user', firstName: 'Marc', lastName: '' },
+    { id: 9, username: 'maité', email: 'maite@npdc.com', role: 'user', firstName: 'Maïté', lastName: '' },
+    { id: 10, username: 'cyril', email: 'cyril@npdc.com', role: 'user', firstName: 'Cyril', lastName: '' },
+    { id: 11, username: 'florian', email: 'florian@npdc.com', role: 'user', firstName: 'Florian', lastName: '' },
+    { id: 12, username: 'alexis', email: 'alexis@npdc.com', role: 'user', firstName: 'Alexis', lastName: '' },
+    { id: 13, username: 'therence', email: 'therence@npdc.com', role: 'user', firstName: 'Thérence', lastName: '' },
+    { id: 14, username: 'eric', email: 'eric@npdc.com', role: 'user', firstName: 'Eric', lastName: '' },
+    { id: 15, username: 'pauline', email: 'pauline@npdc.com', role: 'user', firstName: 'Pauline', lastName: '' },
+    { id: 16, username: 'elisabeth', email: 'elisabeth@npdc.com', role: 'user', firstName: 'Elisabeth', lastName: '' },
+    { id: 17, username: 'olga', email: 'olga@npdc.com', role: 'user', firstName: 'Olga', lastName: '' },
+    { id: 18, username: 'thu', email: 'thu@npdc.com', role: 'user', firstName: 'Thu', lastName: '' },
+    { id: 19, username: 'agathe', email: 'agathe@npdc.com', role: 'user', firstName: 'Agathe', lastName: '' }
   ];
 
   // Mots de passe en dur (en production, utiliser un système de hachage)
   private readonly passwords: { [key: string]: string } = {
     'admin': 'admin123',
-    'user': 'user123',
-    'demo': 'demo123'
+    'alexandra': 'ScT46Omnhgv_',
+    'felix': 'aTBjSO_Q-ezL',
+    'guillaume': '7GZAF343CVV6',
+    'sydney': 'Gc90YXlVQly7',
+    'paul': 'gZoOtfMOcf9v',
+    'kevin': 'LGw5_FCo7AsN',
+    'marc': 'wlC3bG_Vg2gS',
+    'maité': 'JN_D_vZwWhbx',
+    'cyril': 'h_RvK1aND0DS',
+    'florian': 'gFpkXsn_J0-4',
+    'alexis': '-zkJlyA5zuXr',
+    'therence': 'ZKNEepHufd6u',
+    'eric': 'Kmc4D3Gtf74g',
+    'pauline': 'uO3uSWVxcG8v',
+    'elisabeth': 'gV7TToWsLOLd',
+    'olga': '2w5s6ejsjxy4',
+    'thu': 'REe1pbHY692Y',
+    'agathe': 'FlbQ8jZhgjSB'
   };
 
   constructor() {
